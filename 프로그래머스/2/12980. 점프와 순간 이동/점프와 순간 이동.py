0c922dba33f9ffd582977954bf3e92f20c6a7e9c
Kim Hyeran <khr9468@gmail.com>
2024-11-10 13:59:38 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -0,0 +1,11 @@
+def solution(n):
+    battery_usage = 0
+    
+    while n > 0:
+        if n % 2 == 0:  
+            n //= 2
+        else:           
+            n -= 1
+            battery_usage += 1
+    
+    return battery_usage
