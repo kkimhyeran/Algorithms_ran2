9bdc1868da36480a67ef5d0012d2817a4a934814
Kim Hyeran <khr9468@gmail.com>
2024-12-05 16:48:38 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 76.63 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,40 +1,10 @@
 from collections import deque
 def solution(queue1, queue2):
     
-    total = sum(queue1) + sum(queue2)
+    target_val = (sum(queue1) + sum(queue2)) / 2
+    print(target_val)
     
-    if total % 2 == 0 :
-        target_val = total // 2
-    else:
-        return -1
-    
-        
     queue1 = deque(queue1)
     queue2 = deque(queue2)
     
-    q1_sum = sum(queue1)
-    q2_sum = sum(queue2)
-    
-    n = len(queue1)
-    chk = 0
-    
-    while chk < n*3:
-        chk += 1
-        
-        if q1_sum == target_val:
-            return chk-1
-        
-        elif q1_sum > target_val:
-            val = queue1.popleft()
-            queue2.append(val)
-            q1_sum -= val
-            q2_sum += val
-        
-        elif q1_sum < target_val:
-            val = queue2.popleft()
-            queue1.append(val)
-            q2_sum -= val
-            q1_sum += val
-            
-        
-    return -1
+    return 0
