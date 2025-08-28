9e916ca11b9f0a7152756beeec9e879bf8d1b883
Kim Hyeran <khr9468@gmail.com>
2025-08-28 15:42:13 +0900

    [Gold V] Title: 토마토, Time: 3824 ms, Memory: 50416 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,55 +1,43 @@
+import sys
 from collections import deque
-# 1. 입력값
-m, n, h = map(int, input().split(' '))
+input = sys.stdin.readline
 
-box = [[list(map(int, input().split(' '))) for _ in range(n)] for _ in range(h)]
+# 1. 입력 받기
+m, n, h = map(int, input().split())
+tomato_boxes = [[list(map(int, input().split())) for _ in range(n)] for _ in range(h)]
 
-# print(box)
-# 2. 익은 토마토가 있는 위치 뽑기
-q = deque([])
-
-visited = [[[False]*m for _ in range(n)] for _ in range(h)]
-
-for x in range(h):
-    for y in range(n):
-        for z in range(m):
-            # print('x: {} y: {} z:{}'.format(x,y,z))
-            # 익은 토마토이면
-            if box[x][y][z] == 1 and  visited[x][y][z] == False:
-                q.append([x,y,z])
-                visited[x][y][z] = 1 # 방문 처리
-
-# 3. bfs 탐색
+# 6방향 (상, 하, 좌, 우, 위, 아래)
 dx = [-1, 1, 0, 0, 0, 0]
 dy = [0, 0, -1, 1, 0, 0]
 dz = [0, 0, 0, 0, -1, 1]
 
+# 2. BFS 준비 (익은 토마토들 큐에 삽입)
+q = deque()
+for z in range(h):
+    for y in range(n):
+        for x in range(m):
+            if tomato_boxes[z][y][x] == 1:
+                q.append((z, y, x))
+
+# 3. BFS 진행
 while q:
-    x, y, z = q.popleft()
-
+    z, y, x = q.popleft()
     for i in range(6):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        nz = z + dz[i]
+        nz, ny, nx = z + dz[i], y + dy[i], x + dx[i]
+        if 0 <= nz < h and 0 <= ny < n and 0 <= nx < m:
+            if tomato_boxes[nz][ny][nx] == 0:   # 안 익은 토마토만 익힘
+                tomato_boxes[nz][ny][nx] = tomato_boxes[z][y][x] + 1
+                q.append((nz, ny, nx))
 
-        # 박스 범위 체크
-        if nx < 0 or nx >= h or ny < 0 or ny >= n or nz < 0 or nz >= m:
-            continue
-
-        # 아직 익지 않음 토마토 + 방문하지 않은 칸
-        if box[nx][ny][nz] == 0 and visited[nx][ny][nz] == False:
-            box[nx][ny][nz] = box[x][y][z] + 1
-            visited[nx][ny][nz] = True
-            q.append([nx, ny, nz])
-
-# 4. 결과 출력
-rslt = 0
-for b in box:
-    for r in b:
-        for c in r:
-            if c == 0:
+# 4. 결과 계산
+days = 0
+for z in range(h):
+    for y in range(n):
+        for x in range(m):
+            if tomato_boxes[z][y][x] == 0:  # 아직 안 익은 토마토가 있으면
                 print(-1)
-                exit()
-        rslt = max(rslt, max(r))
-        
-print(rslt-1)
+                sys.exit(0)
+            days = max(days, tomato_boxes[z][y][x])
+
+# 5. 정답 출력 (최소 일수)
+print(days - 1)
