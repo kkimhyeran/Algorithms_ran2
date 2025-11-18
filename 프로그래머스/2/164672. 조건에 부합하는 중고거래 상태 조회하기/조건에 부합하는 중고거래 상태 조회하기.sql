59774e41f9db616baeb4a53c31e346d459be4f2d
Kim Hyeran <khr9468@gmail.com>
2025-11-18 11:11:35 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,12 @@
+-- 코드를 입력하세요
+SELECT board_id
+    , writer_id
+    , title
+    , price
+    , CASE WHEN status = 'SALE' THEN '판매중'
+        WHEN status = 'RESERVED' THEN '예약중'
+        WHEN status = 'DONE' THEN '거래완료'
+    END status
+FROM used_goods_board
+WHERE TO_CHAR(created_date, 'yyyy-mm-dd') = '2022-10-05'
+ORDER BY board_id DESC
